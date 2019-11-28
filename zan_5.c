1aa91a84c539dec43448ead047cd130a1a405319
Exxplosion <nikitosfly200093@gmail.com>
2019-11-28 23:47:33 +0300

    add_final_ls(zan_5)

--- zan_5.c
+++ zan_5.c
@@ -16,111 +16,151 @@
 #include <malloc.h>
 #include <stdbool.h>
 #include <pwd.h>
+#include <errno.h>
 
 int stat(const char *file_name, struct stat *buf);
 int fstat(int filedes, struct stat *buf);
 int lstat(const char *file_name, struct stat *buf);
 DIR *opendir(const char *name);
 struct dirent *readdir(DIR *dir);
+void print_ls(bool found_l, bool found_i, bool found_n, struct stat s, char *file_name_);
 
+	// C -a работает некорректно.
 
-void do_ls(char *str, char *file_name) //добавить кол - во жестких ссылок
-										//str - argv[i], 
+void do_ls(char *str, bool found_l, bool found_i, bool found_n, bool found_a, bool found_R)
 {
 	DIR *directory_name;
 	struct dirent *dirent_dir; 
 
 	if ((directory_name = opendir(str)))
 	{
-		printf("%s:	\n", str);
+		printf("\n %s:	\n", str);
 		do
 		{
 			if((dirent_dir = readdir(directory_name)) == NULL)
 			{
-				return;
-				//perror("readdir");
+				if (errno != 0) 
+					perror("readdir");
+				break;
 			}
 
-			if((!(dirent_dir->d_name[0] == '.')))
+			if ((!(dirent_dir->d_name[0] == '.')) || found_a)
 			{
 				struct stat s;
-				char *str_path_name = (char *) calloc(strlen(str) + strlen(file_name) + 2, sizeof(char));
+				char *str_path_name = (char *) calloc(strlen(str) + strlen(dirent_dir->d_name) + 2, sizeof(char));
 				strncpy(str_path_name, str, strlen(str)); //str_path_name = argv[i];
-				strncat(str_path_name, file_name, strlen(file_name));  //'x'  ./Desktop/x;   "x";
+				strncat(str_path_name, dirent_dir->d_name, strlen(dirent_dir->d_name));  //'x'  ./Desktop/x;   "x";
 
 				if (stat(str_path_name, &s) == -1)
 				{
-					printf("\"%s\" AAA\n", str_path_name); 
 					perror("Stat fail");
-					return;
+					break;
 				}
 
-
-				const time_t *timep = &(s.st_atime);
-
-				struct passwd *pwd_1 = getpwuid(s.st_uid);
-				struct passwd *pwd_2 = getpwuid(s.st_gid);
-
-				char root_name[] = "----------";
-
-			    if ( S_ISDIR(s.st_mode) )  root_name[0] = 'd';
-			    if ( S_ISCHR(s.st_mode) )  root_name[0] = 'c';
-			    if ( S_ISBLK(s.st_mode) )  root_name[0] = 'b';
-
-			    if ( s.st_mode & S_IRUSR ) root_name[1] = 'r';
-			    if ( s.st_mode & S_IWUSR ) root_name[2] = 'w';
-			    if ( s.st_mode & S_IXUSR ) root_name[3] = 'x';
-
-			    if ( s.st_mode & S_IRGRP ) root_name[4] = 'r';
-			    if ( s.st_mode & S_IWGRP ) root_name[5] = 'w';
-			    if ( s.st_mode & S_IXGRP ) root_name[6] = 'x';
-
-			    if ( s.st_mode & S_IROTH ) root_name[7] = 'r';
-			    if ( s.st_mode & S_IWOTH ) root_name[8] = 'w';
-			    if ( s.st_mode & S_IXOTH ) root_name[9] = 'x';
-
-				printf("%10ld %s %s %s %ld %s %s\n", s.st_ino, root_name, pwd_1->pw_name, pwd_2->pw_name, s.st_size, ctime(timep), file_name); //ПРимерный вывод с опцией -l
-				
-
-
-				//printf("PATH NAME %s", str_path_name);
-
-				//if ((directory_name = opendir(str_path_name))) //файл есть директория
-				//{
-				//	printf(" HEHEHEHEH:%s 	\n", str_path_name);
-
-				//	if((dirent_dir = readdir(directory_name)))
-				//	{
-				//		if (!(dirent_dir->d_name[0] == '.'))
-				//		{
-							printf(" HEHEHEHEH:%s OHOHO %s	\n", str_path_name, dirent_dir->d_name);
-							strncat(str_path_name, "/", 1);  //'x'  ./Desktop/x;   "x";
-							do_ls(str_path_name, dirent_dir->d_name);
-				//		}
-						//return;
-				//	}
-
+				print_ls(found_l, found_i, found_n, s, dirent_dir->d_name);
 				free(str_path_name);
 			}
 		}
 		while(dirent_dir);
 	}
+	else
+	{
+		if (errno != ENOTDIR) //((errno == EACCES) || (errno == EADDRNOTAVAIL) || (errno == ENOENT)) 
+		{
+			printf("ls: cannot open file: %s\n ", str);
+					perror("opendir");
+		}
+	}
+
+	closedir(directory_name);
+
+	if ((directory_name = opendir(str)) && found_R)
+	{
+		do
+		{
+			if ((dirent_dir = readdir(directory_name)))
+			{
+				if ((!(dirent_dir->d_name[0] == '.')) || (found_a && ((dirent_dir->d_name[1] != '\0') && (dirent_dir->d_name[1] != '.')))) //|| (found_a && (dirent_dir->d_name[1] != '.')))
+				{
+					char *str_path_name = (char *) calloc(strlen(str) + strlen(dirent_dir->d_name) + 3, sizeof(char));
+					strncpy(str_path_name, str, strlen(str));
+					strncat(str_path_name, dirent_dir->d_name, strlen(dirent_dir->d_name)); //str_path_name = argv[i];
+					strncat(str_path_name, "/", 1);	
+					do_ls(str_path_name, found_l, found_i, found_n, found_a, found_R);
+					free(str_path_name);
+				}
+			}
+		}
+		while(dirent_dir);
+	}
+
+	closedir(directory_name);
 }
+
+
+
+void print_ls(bool found_l, bool found_i, bool found_n, struct stat s, char *file_name_)
+{
+	const time_t *timep = &(s.st_atime);
+
+	struct passwd *pwd_1 = getpwuid(s.st_uid);
+	struct passwd *pwd_2 = getpwuid(s.st_gid);
+
+	char root_name[] = "----------";
+
+    if ( S_ISDIR(s.st_mode) )  root_name[0] = 'd';
+    if ( S_ISCHR(s.st_mode) )  root_name[0] = 'c';
+    if ( S_ISBLK(s.st_mode) )  root_name[0] = 'b';
+
+    if ( s.st_mode & S_IRUSR ) root_name[1] = 'r';
+    if ( s.st_mode & S_IWUSR ) root_name[2] = 'w';
+    if ( s.st_mode & S_IXUSR ) root_name[3] = 'x';
+
+    if ( s.st_mode & S_IRGRP ) root_name[4] = 'r';
+    if ( s.st_mode & S_IWGRP ) root_name[5] = 'w';
+    if ( s.st_mode & S_IXGRP ) root_name[6] = 'x';
+
+	if ( s.st_mode & S_IROTH ) root_name[7] = 'r';
+	if ( s.st_mode & S_IWOTH ) root_name[8] = 'w';
+	if ( s.st_mode & S_IXOTH ) root_name[9] = 'x';
+
+	char *time_str = ctime(timep);
+	time_str[strlen(time_str)-1] = '\0';
+	
+	if (found_n)
+	{
+		if (found_i)
+			printf("%ld %s %ld %d %d %6ld %s %s\n", s.st_ino, root_name, s.st_nlink, s.st_uid, s.st_gid, s.st_size, time_str, file_name_);
+		else
+			printf("%s %ld  %d %d %6ld %s %s\n", root_name, s.st_nlink, s.st_uid, s.st_gid, s.st_size, time_str, file_name_);
+		return;
+	}
+
+	if (found_l)
+	{
+		if (found_i)
+			printf("%ld %s %ld %s %s %6ld %s %s\n", s.st_ino, root_name, s.st_nlink, pwd_1->pw_name, pwd_2->pw_name, s.st_size, time_str, file_name_);
+		else
+			printf("%s %ld %s %s %6ld %s %s\n", root_name, s.st_nlink, pwd_1->pw_name, pwd_2->pw_name, s.st_size, time_str, file_name_);
+		return;
+	}
+
+	if (found_i)
+			printf("%ld %s\n", s.st_ino, file_name_);
+	else
+			printf("%s\n", file_name_);
+	return;
+} 
 
 int main(int argc, char *argv[])
 {
-	//struct passwd *pwd_1, *pwd_2; //для определения имени пользователя и группы (без них выводит как с функцией -n)
-					struct stat s;
+	struct stat s;
 
 	bool found_a = false;
 	bool found_l = false;
 	bool found_i = false;
 	bool found_n = false;
-	bool found_d = false;
 	bool found_R = false;
-
-	char root_name[11]; //(char *) malloc(10*sizeof(char)); //переводит числовое значчение рут дотсупа в строчное (см далее)
-	char *str_path_name;
 
 	int i;
 	int opt;
@@ -130,7 +170,7 @@ int main(int argc, char *argv[])
 	
 
 
-	while((opt = getopt(argc, argv, "lanidR:")) != -1) 
+	while((opt = getopt(argc, argv, "lanidR")) != -1) 
 	{
 		switch(opt)
 		{
@@ -142,9 +182,6 @@ int main(int argc, char *argv[])
 				break;
 			case 'i':
 				found_i = true;
-				break;
-			case 'd':
-				found_d = true;
 				break;
 			case 'R':
 				found_R = true;
@@ -175,80 +212,10 @@ int main(int argc, char *argv[])
 
 	for (i = optind; i < argc; i++)
 	{
-		/*
-		if ((directory_name = opendir(argv[i])))
-		{
-			printf("%s:	\n", argv[i]);
-			do
-			{
-				if((dirent_dir = readdir(directory_name)) == NULL)
-				{
-					break;
-				}
-
-				if((!(dirent_dir->d_name[0] == '.')) || found_a)
-				{	
-			*/
-
-
-				do_ls(argv[i], dirent_dir->d_name);
-
-
-
-
-
-
-
-
-
-
-
-				/*
-					str_path_name = (char *) calloc(strlen(argv[i]) + strlen(dirent_dir->d_name) + 2, sizeof(char));
-					strncpy(str_path_name, argv[i], strlen(argv[i])); //str_path_name = argv[i];
-					strncat(str_path_name, dirent_dir->d_name, strlen(dirent_dir->d_name));  //'x'  ./Desktop/x;   "x";
-
-					if (stat(str_path_name, &s) == -1)
-					{
-						printf("/%s/ AAA\n", dirent_dir->d_name); 
-						perror("Stat fail");
-					}
-
-					const time_t *timep = &(s.st_atime);
-
-						pwd_1 = getpwuid(s.st_uid);
-						pwd_2 = getpwuid(s.st_gid);
-
-					strcpy( root_name, "----------" );
-
-				    if ( S_ISDIR(s.st_mode) )  root_name[0] = 'd';
-				    if ( S_ISCHR(s.st_mode) )  root_name[0] = 'c';
-				    if ( S_ISBLK(s.st_mode) )  root_name[0] = 'b';
-
-				    if ( s.st_mode & S_IRUSR ) root_name[1] = 'r';
-				    if ( s.st_mode & S_IWUSR ) root_name[2] = 'w';
-				    if ( s.st_mode & S_IXUSR ) root_name[3] = 'x';
-
-				    if ( s.st_mode & S_IRGRP ) root_name[4] = 'r';
-				    if ( s.st_mode & S_IWGRP ) root_name[5] = 'w';
-				    if ( s.st_mode & S_IXGRP ) root_name[6] = 'x';
-
-				    if ( s.st_mode & S_IROTH ) root_name[7] = 'r';
-				    if ( s.st_mode & S_IWOTH ) root_name[8] = 'w';
-				    if ( s.st_mode & S_IXOTH ) root_name[9] = 'x';
-
-					printf("%10ld %s %s %s %ld %s %s\n", s.st_ino, root_name, pwd_1->pw_name, pwd_2->pw_name, s.st_size, ctime(timep), dirent_dir->d_name); //ПРимерный вывод с опцией -l
-					free(str_path_name);
-					*/
-			//	}
-			//}
-			//while (dirent_dir);
-			printf("\n");
-		}
-	//}
+		do_ls(argv[i], found_l,  found_i, found_n, found_a, found_R);
+		printf("\n");
+	}
 }
-
-
 
 //struct stat {
 //    dev_t         st_dev;      /* устройство */
